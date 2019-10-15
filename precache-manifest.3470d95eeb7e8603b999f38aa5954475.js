4211616cd33aa57a9781c15b4ed337280fcb56cc
Kris Renaldi <krenaldi@comcast.net>
2019-10-15 17:49:15 -0400

    Updates

---
+++ precache-manifest.3470d95eeb7e8603b999f38aa5954475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0a99866d526186343547f08d4e48c3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "bc7cc96c2e7071b941e2",
+    "url": "/clicky-game/static/css/main.b6330ccd.chunk.css"
+  },
+  {
+    "revision": "d84dfe67aa6f4c0cf283",
+    "url": "/clicky-game/static/js/2.46ec92cf.chunk.js"
+  },
+  {
+    "revision": "bc7cc96c2e7071b941e2",
+    "url": "/clicky-game/static/js/main.5c810585.chunk.js"
+  },
+  {
+    "revision": "61fe689653f34435d57f",
+    "url": "/clicky-game/static/js/runtime-main.8c94c7f2.js"
+  }
+]);
